86d6dd94dbdf0ad75c5153c05feb174830032a26
ReaLac5 <realackovic5@gmail.com>
2025-01-20 16:21:55 +0100

    GA

--- javascript/auth.js
+++ javascript/auth.js
@@ -123,29 +123,23 @@ function updateSigninStatus(isSignedIn) {
 
 const CLIENT_ID = '214036241518-be5frrk0bus3h05oo3dt6b2t1j19onr3.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyAJugTkVuqv5BbYEPm9rr7U4mPSA5fvdmI';
-const DISCOVERY_DOCS = ["https://analyticsdata.googleapis.com/$discovery/rest?version=v1"];
+const DISCOVERY_DOCS =
+    ["https://analyticsdata.googleapis.com/$discovery/rest?version=v1"];
 const SCOPES = "https://www.googleapis.com/auth/analytics.readonly";
-
 function handleAuthClick() {
-  gapi.auth2.getAuthInstance().signIn();
+    gapi.auth2.getAuthInstance().signIn();
 }
-
 function handleSignoutClick() {
-  gapi.auth2.getAuthInstance().signOut();
+    gapi.auth2.getAuthInstance().signOut();
 }
-
 function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(() => {
-    console.log("Google API spreman!");
-    document.getElementById("signinButton").style.display = 'none';
-    document.getElementById("signoutButton").style.display = 'inline-block';
-    fetchAnalyticsData();
-  });
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(() => {
+        console.log("Google API spreman!");
+    });
 }
-
 gapi.load("client:auth2", initClient);
