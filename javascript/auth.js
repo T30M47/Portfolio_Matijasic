176c600f92dea7b92420e8b712c5992b7371d62f
ReaLac5 <realackovic5@gmail.com>
2025-01-20 10:38:07 +0100

    GA

--- javascript/auth.js
+++ javascript/auth.js
@@ -25,13 +25,15 @@ const CLIENT_ID = '214036241518-be5frrk0bus3h05oo3dt6b2t1j19onr3.apps.googleuser
 const API_KEY = 'AIzaSyAJugTkVuqv5BbYEPm9rr7U4mPSA5fvdmI';
 const DISCOVERY_DOCS = ["https://analyticsdata.googleapis.com/$discovery/rest?version=v1"];
 const SCOPES = "https://www.googleapis.com/auth/analytics.readonly";
+const REDIRECT_URI = 'https://eportfoliomatijasic.netlify.app/html/chart.html';
 
 function initClient() {
     gapi.client.init({
         apiKey: API_KEY,
         clientId: CLIENT_ID,
         discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
+        scope: SCOPES,
+        redirectUri: REDIRECT_URI
     }).then(() => {
         console.log("Google API spreman!");
         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
